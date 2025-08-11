9ba0963b5c85c5d0273eb37d3ada4ed05f6c4f40
An-k-ush <sounankush123@gmail.com>
2025-08-11 17:51:31 +0530

    Configured relationships between Entities and application.properties, Added MySQL dependency

---
+++ src/main/java/org/myproject/shoppingcart/model/Category.java
@@ -0,0 +1,27 @@
+package org.myproject.shoppingcart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
